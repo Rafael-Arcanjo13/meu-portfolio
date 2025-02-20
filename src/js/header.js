6479cb18b4917c2151cfd5e3fa2d9d14d5685d2c
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-02-19 23:50:42 -0300

    Acessibilidade configurada

--- src/js/header.js
+++ src/js/header.js
@@ -5,3 +5,16 @@ btnMobile.addEventListener('click', () => {
     btnMobile.classList.toggle('animation-btn')
     navList.classList.toggle('active');
 })
+btnMobile.addEventListener('touchstart', (event) => {
+    event.preventDefault();
+    const navList = document.querySelector('.nav-itens');
+    const active = navList.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    btnMobile.classList.toggle('animation-btn');
+    navList.classList.toggle('active');
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
+})
